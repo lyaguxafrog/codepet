9820ab6dd2f99cf748fdd53de488061956f8ce5a
lyaguxafrog <maccree.a@yandex.ru>
2024-04-03 11:49:16 +0300

    chore: сервис создания пользователя

---
+++ kernel/users/services/create_profile.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+
+from typing import Optional
+
+from django.contrib.auth.models import User
+from django.contrib.auth.validators import ASCIIUsernameValidator
+from django.contrib.auth.password_validation import validate_password
+from django.core.validators import validate_email
+from django.db.transaction import atomic
+from django.contrib.auth.models import User
+
+from users.models import UserProfile
+
+validate_username = ASCIIUsernameValidator()
+
+
+@atomic
+def create_profile(
+        username: str,
+        email: str,
+        password: str,
+        repeat_password: str,
+        first_name: str,
+        last_name: str,
+        patronymic: Optional[str]
+    ) -> UserProfile:
+    """
+    Сервис для создания профиля пользователя
+
+    :param username: Имя пользователя, проходит `ASCIIUsernameValidator`
+    :param email: E-mail пользователя, проходит `validate_email`
+    :param password: Пароль пользователя, проходит `validate_password`
+    :param repeat_password: Повторный ввод пароля, сравнивается с `password`
+    :param first_name: Имя
+    :param last_name: Фамилия
+    :param patronymic: Отчество, необязательный параметр
+    """
+
+    # проверки введенных данных на валидность
+    if repeat_password != password:
+        raise Exception('Пароли не совпадают')
+
+    validate_password(password=password)
+    validate_email(email)
+    validate_username(username)
+
+    user = User.objects.create_user(
+        username=username,
+        email=email,
+        password=password,
+        first_name=first_name,
+        last_name=last_name,
+    )
+
+
+    user_profile = UserProfile.objects.create(
+        user=user,
+        first_name=first_name,
+        last_name=last_name,
+        patronymic=patronymic,
+    )
+
+    return user_profile
