03b4285a651e1c1820d7b061cd129d597904f690
lyaguxafrog <maccree.a@yandex.ru>
2024-04-03 19:46:58 +0300

    chore: сервис для платежа
    пока без учета суммы и числа пользователей

---
+++ kernel/fundraising/services/pay_services.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+from typing import Union
+
+from django.db.transaction import atomic
+
+from fundraising.models import Collect, Payment
+from users.models import UserProfile
+
+
+@atomic
+def pay(
+        pay_to: Collect,
+        payer: UserProfile,
+        sum: float
+    ) -> Union[Payment, Exception]:
+    """
+    Функция платежа
+
+    :param pay_to: Объект `Collect` куда идет платеж
+    :param payer: Объект `UserProfile`, тот кто платит
+    :param sum: Сумма платежа
+
+    :return: Созданный платеж или ошибка
+    """
+
+    if pay_to.status == False:
+        raise Exception("Сбор закрыт")
+
+    payment = Payment.objects.create(
+        pay_to=pay_to,
+        payer=payer,
+        sum=sum
+    )
+
+    return payment
