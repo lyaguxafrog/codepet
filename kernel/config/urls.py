f23e538f8381e6c7faeee5637357418b1cb8b0de
lyaguxafrog <maccree.a@yandex.ru>
2024-04-03 12:53:50 +0300

    fix: исправил сериализатор

--- kernel/config/urls.py
+++ kernel/config/urls.py
@@ -5,10 +5,21 @@ from django.urls import path, re_path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from drf_spectacular.views import (SpectacularAPIView,
+                                   SpectacularRedocView,
+                                   SpectacularSwaggerView)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    # Optional UI:
+    path('api/schema/swagger/',
+         SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/schema/redoc/',
+         SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
+
 
 urlpatterns += [
     path('api/users/', include('users.urls')),
--- kernel/users/serializers/profile_serializers.py
+++ kernel/users/serializers/profile_serializers.py
@@ -1,44 +1,39 @@
 # -*- coding: utf-8 -*-
 
 from rest_framework import serializers
-from django.contrib.auth.models import User
 
 from users.services.create_profile import create_profile
 
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = [
-            'username',
-            'email',
-            'password',
-            'repeat_password',
-            'first_name',
-            'last_name',
-            'patronymic'
-            ]
+class UserSerializer(serializers.Serializer):
+    """
+    Создание пользователя
+    """
+    username = serializers.CharField(max_length=100)
+    email = serializers.EmailField()
+    password = serializers.CharField(write_only=True)
+    repeat_password = serializers.CharField(write_only=True)
+    first_name = serializers.CharField(max_length=100)
+    last_name = serializers.CharField(max_length=100)
+    patronymic = serializers.CharField(max_length=100, required=False)
 
-        extra_kwargs = {
-            'password':
-                {'write_only': True},
 
-            'repeat_password':
-                {'write_only': True}
-        }
+    def validate(self, data):
+        if data.get('password') != data.get('repeat_password'):
+            raise serializers.ValidationError("Пароли не совпадают")
+        return data
 
     def create(self, validated_data):
         # Извлекаем пароль из валидированных данных
         password = validated_data.pop('password', None)
-        repeat_password = validated_data.pop('repeat_password', None)
-
         # Создаем пользователя и профиль
         user_profile = create_profile(
             username=validated_data['username'],
             email=validated_data['email'],
             password=password,
-            repeat_password=repeat_password,
+            repeat_password=password,
             first_name=validated_data['first_name'],
             last_name=validated_data['last_name'],
             patronymic=validated_data.get('patronymic', None)
         )
+
         return user_profile.user
