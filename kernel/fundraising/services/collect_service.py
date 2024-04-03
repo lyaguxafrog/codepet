ab0e8a745f24fdf9c4e9aff05dffa93ed2346cca
lyaguxafrog <maccree.a@yandex.ru>
2024-04-03 19:07:13 +0300

    Сервис по остановке сбора

--- kernel/fundraising/services/collect_service.py
+++ kernel/fundraising/services/collect_service.py
@@ -1,10 +1,29 @@
 # -*- coding: utf-8 -*-
 
-
-from typing import Optional
-from datetime import datetime
-
 from django.db.transaction import atomic
-from users.models import UserProfile
 
+from users.models import UserProfile
 from fundraising.models import Collect
+
+@atomic
+def stop_collect(
+        collect: Collect,
+        owner: UserProfile
+    ) -> Collect:
+    """
+    Функция для остановки сбора.
+
+    :param collect: Экземпляр сбора, который нужно остановить.
+    :param owner: Владелец сбора, выполняющий операцию.
+    :return: Обновленный экземпляр сбора с измененным статусом.
+    """
+
+    # Проверяем, что владелец сбора является тем, кто пытается остановить сбор
+    if collect.collect_owner != owner:
+        raise Exception('Только владелец сбора может остановить его.')
+
+    # Изменяем статус сбора на False
+    collect.status = False
+    collect.save()
+
+    return collect
