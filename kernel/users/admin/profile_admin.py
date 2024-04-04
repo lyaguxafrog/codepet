aa7d3240a1bac1ae0656732b2ed3188e413760e7
lyaguxafrog <maccree.a@yandex.ru>
2024-04-04 11:58:41 +0300

    chore: убрал удаление и добавление пользователей

--- kernel/users/admin/profile_admin.py
+++ kernel/users/admin/profile_admin.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from django.contrib import admin
+from django.http import HttpRequest
 
 from users.models import UserProfile
 
@@ -15,10 +16,7 @@ class ProfileAdmin(admin.ModelAdmin):
         'create_date',
     ]
 
-    readonly_fields = [
-        'user',
-        'create_date',
-    ]
+    readonly_fields = fields
 
     list_display = [
         'user',
@@ -26,3 +24,20 @@ class ProfileAdmin(admin.ModelAdmin):
         'last_name',
         'create_date',
     ]
+
+    search_fields = [
+        'user',
+        'first_name',
+        'last_name',
+        'patronymic',
+        'create_date',
+    ]
+
+
+    def has_delete_permission(
+        self, request: HttpRequest, obj: UserProfile = None,
+    ) -> bool:
+        return False
+
+    def has_add_permission(self, requst: HttpRequest) -> bool:
+        return False
